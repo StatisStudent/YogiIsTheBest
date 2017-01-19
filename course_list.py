b51b85777192d230a0562e91e6fc2009feb42ed3
Ido Haber <ido>
2017-01-19 18:07:50 +0200

    bug fixes

--- course_list.py
+++ course_list.py
@@ -1,6 +1,8 @@
+import pdb
 import urllib2
 from bs4 import BeautifulSoup
 import unicodedata
+import easygui
 
 
 def get_course_list():
@@ -12,3 +14,28 @@ def get_course_list():
         courses.append(unicodedata.normalize('NFKD', a.getText()).encode('ascii', 'ignore'))
 
     return courses
+
+
+def get_name_of_courses(course_number):
+    url = 'https://webcourse.cs.technion.ac.il/' + course_number
+    soup = BeautifulSoup(urllib2.urlopen((url)), 'html.parser')
+    name = ""
+    for meta in soup.findAll('meta'):
+        if u'name' in meta.attrs and meta.attrs[u'name'] == u'Description':
+            name = unicodedata.normalize('NFKD', meta.attrs[u'content'].split(u',')[1]).encode('ascii', 'ignore').strip()
+            break
+    if name == '':
+        for span in soup.findAll('span'):
+            if u'class' in span.attrs and span.attrs[u'class'][0] == u'titlebarname':
+                try:
+                    name = span.contents[3].contents[0]
+                except:
+                    name = span.contents[3].attrs[u'data-lang-en']
+                name = name[len('234123'):].strip().replace(u"-","")
+                break
+
+
+    return name
+if __name__ == 'main':
+    for c in get_course_list():
+        print (get_name_of_courses(c))
--- gui_file.py
+++ gui_file.py
@@ -1,20 +1,26 @@
 import course_scrapper
 import easygui as g
 import sys
-from course_list import get_course_list
+from course_list import *
 
 msg ="Which courses do you take?"
 title = "WebScraper"
 choices = get_course_list()
+names = map(lambda x: get_name_of_courses(x), choices )
+choices =  map(lambda x: u', '.join((x[0], x[1])).encode('utf-8').strip() , zip(choices,names))
+# print type(choices[0][0]),int(choices[0][0])
+# for x in choices:
+#     print x[0],x
+# sys.exit(0)
 choice = g.multchoicebox(msg, title, choices)
 course_output = ""
 for x in choice:
     try:
-        course_output += course_scrapper.CoursePage(int(x)).get_assignment_data
+        course_output += course_scrapper.CoursePage(int(x[:6])).get_assignment_data
         course_output += "\n"
     except Exception:
-        print int(x)
-        sys.exit(0)
+        print int(x[0])
+
 
 g.textbox("Here are Your upcoming submissions:", "Courses Result",  course_output)
 
